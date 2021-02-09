97393766c7ecc40ee8c51ba117f0e976e0579a30
Mrbabibo <Mrbabibo@163.com>
2021-02-09 17:08:12 +0800

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,46 @@
+﻿// 打开后台页
+$('.btn0').click(e => {
+ 
+  window.open('https://mail.postoa.com.cn/');
+});
+$('.btn1').click(e => {
+  window.open('https://office.postoa.com.cn/jt/login.jsp');
+});
+$('.btn2').click(e => {
+  window.open('https://211.156.194.132/jsp/indexpage/index_work_frameset.jsp?modelID=027');
+});
+$('.btn3').click(e => {
+  window.open('https://www.cpoc.cn/');
+});
+$('.btn4').click(e => {
+  window.open('https://docs.qq.com/sheet/DV2VRUVJxaFpVaWN3?tab=gq4hkp&friendUin=LaactCm%25252BCM0301IblAGyhA%25253D%25253D&ADUIN=1522665612&ADSESSION=1608186633&ADTAG=CLIENT.QQ.5785_.0&ADPUBNO=27077&jumpuin=1522665612');
+});
+$('.btn5').click(e => {-
+  window.open('https://docs.qq.com/sheet/DR3FyR1VRSWl4UU1W?tdsourcetag=s_pcqq_send_grpfile&ADUIN=1522665612&ADSESSION=1612313490&ADTAG=CLIENT.QQ.5791_.0&ADPUBNO=27099&tab=BB08J2');
+});
+$('.btn6').click(e => {
+  window.open('https://192.168.112.177/portal/a');
+});
+$('.btn7').click(e => {
+  window.open('https://lanhuapp.com/web/#/item/project/stage?pid=13e9eb3e-6ec7-4116-8ca9-7b73b8c59469');
+});
+$('.btn8').click(e => {
+  window.open('https://docs.qq.com/sheet/DRHJnZ1pOeGNCU0lV?tab=BB08J8');
+});
+$('.btn9').click(e => {
+  window.open('https://shimo.im/docs/drRCKQJW3VdyJ3dY');
+});
+$('.btn10').click(e => {
+  window.open('https://211.156.200.177/jsp/employeeHome.jsp');
+});
+$('.btn11').click(e => {
+  window.open('https://211.156.194.132/jsp/index_oldblue.jsp#');
+});
+$('.btn12').click(e => {
+  window.open('https://shimo.im/docs/hdJchG69TwJcGKQX');
+});
+
+
+// setTimeout(()=>{
+//   alert(1111)
+// },1000)
